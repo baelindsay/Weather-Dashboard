fa610f9b9895b53430255ad697b9407aed403802
baelindsay <109304038+baelindsay@users.noreply.github.com>
2022-09-06 14:01:47 -0400

    add javascript sc in script added apiKey URL

---
+++ javascript.js
@@ -0,0 +1,6 @@
+// API Key //
+var apiKey = 'a1592b841ec303cddcf1f50344cedc17';
+
+// URL for present day (city name + weather) //
+var URL_Weather = "https://api.openweathermap.org/data/2.5/onecall? lat={lat}&lon={lon}&exclude={part}&appid={API key}"
+
