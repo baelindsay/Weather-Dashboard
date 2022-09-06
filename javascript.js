8dedc808fe59e8d0b02831635b6475a9859f9a7b
baelindsay <109304038+baelindsay@users.noreply.github.com>
2022-09-06 15:01:39 -0400

    add and edit URL weather + forecast

--- javascript.js
+++ javascript.js
@@ -1,6 +1,14 @@
 // API Key //
 var apiKey = 'a1592b841ec303cddcf1f50344cedc17';
 
+// List of DOM Elements //
+var searchBtn = document.querySelector('.scavenge');
+var cityList = document.querySelector('.city-list');
+var input = document.querySelector('.input');
+
 // URL for present day (city name + weather) //
-var URL_Weather = "https://api.openweathermap.org/data/2.5/onecall? lat={lat}&lon={lon}&exclude={part}&appid={API key}"
+var URL_Weather = "https://api.openweathermap.org/data/2.5/onecall?" + city_name + '&units=imperial' + apiKey
+
+// URL for 5-days forcast (city name + weather)//
+var URL_Forecast = "https://api.openweathermap.org/data/2.5/onecall?" + city_name + '&units=imperial' + apiKey
 
